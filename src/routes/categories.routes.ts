b3ecde554c4eb7e9563b516bc4df8c78dc7c70c4
Fernando <fernando39357@gmail.com>
2022-02-24 14:12:07 -0300

    feat: categories route (create and listAll)

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
+
+const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
+
+categoriesRoutes.post('/', (request, response) => {
+  const { name, description } = request.body;
+
+  categoriesRepository.create({ name, description });
+
+  return response.sendStatus(201);
+});
+
+categoriesRoutes.get('/', (request, response) => {
+  const categories = categoriesRepository.index();
+
+  return response.json(categories);
+});
+
+export { categoriesRoutes };
