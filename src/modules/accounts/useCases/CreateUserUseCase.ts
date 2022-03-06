1c4f03191937e35f14ff976361b2d057c2ad7f1a
Fernando <fernando39357@gmail.com>
2022-03-06 14:21:50 -0300

    feat: user validation

--- src/modules/accounts/useCases/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/CreateUserUseCase.ts
@@ -17,6 +17,10 @@ class CreateUserUseCase {
     password,
     driver_license,
   }: ICreateUserDTO): Promise<void> {
+    const userAlreadExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadExists) throw new Error('User already exists');
+
     const passwordHash = await hash(password, 8);
 
     await this.usersRepository.create({
